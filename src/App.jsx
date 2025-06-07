a747e3f9ae4affc33f040154ba02cfcd7b560f10
haunc0716 <haunc0716@gmail.com>
2025-06-07 10:19:32 +0700

    Adding UI: Login, Register

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,76 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Layout from "./components/Layout"
-import LayoutDashboard from "./components/LayoutDashboard"
-import Homepage from "./pages/Homepage"
-import Dashboard from "./pages/Dashboard"
-import UserManagement from "./pages/UserManagement"
-import ConsultantManagement from "./pages/ConsultantManagement"
-import Profile from "./pages/Profile"
+import Layout from "./components/Layout";
+import LayoutDashboard from "./components/LayoutDashboard";
+import Homepage from "./pages/Homepage";
+import Dashboard from "./pages/Dashboard";
+import UserManagement from "./pages/UserManagement";
+import ConsultantManagement from "./pages/ConsultantManagement";
+import Profile from "./pages/Profile";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
-
   return (
-     <BrowserRouter>
-          <Routes>
-              <Route path="/home" element={<Layout><Homepage /></Layout>} />
-              <Route path="/dashboard" element={<LayoutDashboard activeMenuItem="overview"><Dashboard /></LayoutDashboard>} />
-              <Route path="/usermanagement" element={<LayoutDashboard activeMenuItem="usermanagement"><UserManagement /></LayoutDashboard>} />
-              <Route path="/consultantmanagement" element={<LayoutDashboard activeMenuItem="consultantmanagement"><ConsultantManagement /></LayoutDashboard>} />
-              <Route path="/profile" element={<LayoutDashboard activeMenuItem="profile"><Profile /></LayoutDashboard>} />
-          </Routes>
-      </BrowserRouter>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/home"
+          element={
+            <Layout>
+              <Homepage />
+            </Layout>
+          }
+        />
+        <Route
+          path="/dashboard"
+          element={
+            <LayoutDashboard activeMenuItem="overview">
+              <Dashboard />
+            </LayoutDashboard>
+          }
+        />
+        <Route
+          path="/usermanagement"
+          element={
+            <LayoutDashboard activeMenuItem="usermanagement">
+              <UserManagement />
+            </LayoutDashboard>
+          }
+        />
+        <Route
+          path="/consultantmanagement"
+          element={
+            <LayoutDashboard activeMenuItem="consultantmanagement">
+              <ConsultantManagement />
+            </LayoutDashboard>
+          }
+        />
+        <Route
+          path="/profile"
+          element={
+            <LayoutDashboard activeMenuItem="profile">
+              <Profile />
+            </LayoutDashboard>
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            <Layout>
+              <Login />
+            </Layout>
+          }
+        />
+        <Route
+          path="/register"
+          element={
+            <Layout>
+              <Register />
+            </Layout>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
