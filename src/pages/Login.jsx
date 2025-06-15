7999c20bcb94d2c3b7386ee002602af090cbfc11
haunc0716 <haunc0716@gmail.com>
2025-06-15 14:56:53 +0700

    Update profile

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -122,7 +122,7 @@ const Login = () => {
               </label>
               <div className="relative">
                 <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-                  <Lock className="h-5 w-5 text-gray-400" />
+                  <Lock className="h- 5 w-5 text-gray-400" />
                 </div>
                 <input
                   id="password"
